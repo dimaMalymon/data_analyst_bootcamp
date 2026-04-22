d786c07d9940906a0ac4f4d7be8a11e11daa7c4b
Dmytro Malymon <44093814+dimaMalymon@users.noreply.github.com>
2026-04-22 14:39:33 +0200

    New bootcamp project

---
+++ Cleaning_date.sql
@@ -0,0 +1,181 @@
+DROP TABLE IF EXISTS layoffs;
+CREATE TABLE layoffs(
+	company TEXT,
+    location TEXT,
+    industry TEXT,
+    total_laid_off TEXT,
+    percentage_laid_off TEXT,
+    `date` TEXT,
+    stage TEXT,
+    country TEXT,
+    funds_raised_millions TEXT
+);
+
+SHOW VARIABLES LIKE 'local_infile';
+SET GLOBAL local_infile = ON;
+
+LOAD DATA LOCAL INFILE '/Users/dmlogistic/Downloads/layoffs.csv'
+INTO TABLE layoffs
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+SELECT *
+FROM layoffs
+;
+
+
+CREATE TABLE layoffs_staging
+LIKE layoffs;
+
+SELECT * 
+FROM layoffs_staging
+WHERE company = 'Casper'
+;
+
+INSERT layoffs_staging
+SELECT *
+FROM layoffs;
+
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, industry, total_laid_off,percentage_laid_off,  `date` ) AS row_num
+FROM layoffs_staging;
+
+
+WITH duplicate_cte AS
+(SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company,location, 
+industry, total_laid_off,percentage_laid_off,  `date`, stage,
+ country, funds_raised_millions ) AS row_num
+FROM layoffs_staging 
+)
+SELECT *
+FROM duplicate_cte
+WHERE row_num > 1;
+
+SELECT * 
+FROM layoffs_staging
+WHERE company = 'Casper'
+;
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` text,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` text,
+  `row_num` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci; 
+
+
+SELECT * 
+FROM layoffs_staging2
+;
+
+INSERT INTO layoffs_staging2
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company,location, 
+industry, total_laid_off,percentage_laid_off,  `date`, stage,
+ country, funds_raised_millions ) AS row_num
+FROM layoffs_staging 
+;
+
+
+DELETE 
+FROM layoffs_staging2
+WHERE row_num >1;
+
+
+
+SELECT company, TRIM(company) 
+FROM layoffs_staging2
+;
+
+UPDATE layoffs_staging2
+SET company = TRIM(company);
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry LIKE 'Crypto%'
+;
+
+UPDATE layoffs_staging2
+SET Industry = 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country)
+WHERE country LIKE 'United States%';
+
+
+UPDATE layoffs_staging2
+SET `date` = str_to_date(`date`, '%m/%d/%Y');
+
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL
+;
+
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = ''
+;
+
+SELECT *
+FROM layoffs_staging2
+WHERE company = "Bally's Interactive";
+
+UPDATE layoffs_staging2
+SET industry = NULL
+WHERE industry = '';
+
+SELECT t1.industry, t2.industry 
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+    AND t1.location = t2.location
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL ;
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL
+;
+
+DELETE
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL
+;
+
+
+SELECT *
+FROM layoffs_staging2;
+
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
